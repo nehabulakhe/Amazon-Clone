37e97969e425c2faf9e38298712ac5b44b0c06ef
Neha Bulakhe <nehabulakhe0922@gmail.com>
2023-09-20 14:55:35 +0530

    Initial Navbar building

--- src/Component/Header.js
+++ src/Component/Header.js
@@ -1,13 +1,46 @@
 import React from 'react';
+import "../Css/Header.css";
+import SearchIcon from '@mui/icons-material/Search';
+import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
 
 function Header() {
     return (
         <div className='header'>
-            <img src="/logo.jpg" />
-
+            <img className='header_logo' src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" alt="logo" />
             <div className='header_search'>
-                <input type="text" />
-                <img src="" alt="" />
+                <input type="text" className='header_searchInput' />
+                <SearchIcon className='header_searchIcon' />
+            </div>
+            <div className="header_nav">
+                <div className="header__option">
+                    <span className="header__optionLineOne">
+                        Hello guest
+                    </span>
+                    <span className="header__optionLineTwo">
+                        Sign In
+                    </span>
+                </div>
+                <div className="header__option">
+                    <span className="header__optionLineOne">
+                        Returns
+                    </span>
+                    <span className="header__optionLineTwo">
+                        & Orders
+                    </span>
+                </div>
+                <div className="header__option">
+                    <span className="header__optionLineOne">
+                        Your
+                    </span>
+                    <span className="header__optionLineTwo">
+                        Prime
+                    </span>
+                </div>
+                <div className="header_optionBasket">
+                    <ShoppingBasketIcon />
+                    <span className="header_optionLineTwo header_basketCount">0</span>
+                </div>
+
             </div>
         </div>
     )
